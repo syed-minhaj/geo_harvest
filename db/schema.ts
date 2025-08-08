d1221be054c03dc4a784c584655baf5000bfde29
syed-minhaj <minhajsulmuneeb@gmail.com>
2025-08-08 15:58:27 +0500

    build: add better auth

--- db/schema.ts
+++ db/schema.ts
@@ -1,8 +1,3 @@
 
 import { pgTable, serial, text, timestamp } from "drizzle-orm/pg-core";
 
-export const users = pgTable("users", {
-  id: serial("id").primaryKey(),
-  name: text("name").notNull(),
-  createdAt: timestamp("created_at").defaultNow(),
-});
