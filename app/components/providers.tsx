1a1149bcddd92844cb903f18ada82eb33465dec3
syed-minhaj <minhajsulmuneeb@gmail.com>
2025-08-08 16:05:39 +0500

    build: add ui for auth (@daveyplate/better-auth-ui) and shadcn

---
+++ app/components/providers.tsx
@@ -0,0 +1,34 @@
+"use client"
+
+import { AuthUIProvider } from "@daveyplate/better-auth-ui"
+import Link from "next/link"
+import { useRouter } from "next/navigation"
+import type { ReactNode } from "react"
+import { Toaster } from "./ui/sonner";
+
+import { authClient } from "../lib/auth-client"
+
+export default function Providers({ children }: { children: ReactNode }) {
+    const router = useRouter()
+
+    return (
+        <AuthUIProvider
+            authClient={authClient}
+            navigate={router.push}
+            replace={router.replace}
+            onSessionChange={() => {
+                router.refresh()
+            }}
+            Link={Link}
+            social={{providers : ["google"]}}
+            credentials={{
+                passwordValidation: {
+                    minLength: 8,
+                },
+                confirmPassword: true,
+            }}
+        >
+            {children}
+        </AuthUIProvider>
+    )
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import 'leaflet/dist/leaflet.css';
-import {db} from './lib/drizzle';
-import {users} from '../db/schema';
-
+import  Providers from "./components/providers";
+import { Toaster } from "./components/ui/sonner";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,11 +27,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+        <body
+            className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        >
+            <Providers>
+                {children}
+            </Providers>
+            <Toaster />
+        </body>
     </html>
   );
 }
