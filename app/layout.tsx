824fba6a42db029cfd0e5c53af0816ce9d654187
syed-minhaj <minhajsulmuneeb@gmail.com>
2025-08-08 11:37:21 +0500

    create project

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,10 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import 'leaflet/dist/leaflet.css';
+import {db} from './lib/drizzle';
+import {users} from '../db/schema';
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
