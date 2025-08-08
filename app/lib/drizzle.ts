e6b3c258157a461449561ad9bd49edb92f1b86a1
syed-minhaj <minhajsulmuneeb@gmail.com>
2025-08-08 11:40:04 +0500

    add drizzle

---
+++ app/lib/drizzle.ts
@@ -0,0 +1,8 @@
+// src/db/index.ts
+import { drizzle } from "drizzle-orm/neon-http";
+import { neon } from "@neondatabase/serverless";
+import * as schema from "../../db/schema";
+
+const sql = neon(process.env.DATABASE_URL!);
+
+export const db = drizzle(sql, { schema });
