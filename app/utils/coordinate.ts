b3275df3dbb2fd77176524b5b025389da8880ef6
syed-minhaj <minhajsulmuneeb@gmail.com>
2025-08-09 15:41:49 +0500

    get user location coordinates

---
+++ app/utils/coordinate.ts
@@ -0,0 +1,36 @@
+
+type location = {
+    latitude : number,
+    longitude : number
+}
+
+const getUserLocation = () : Promise<{location : location , err : null} | {location : null , err : string}> => {
+    return new Promise((resolve) => {
+        if ("geolocation" in navigator) {
+            navigator.geolocation.getCurrentPosition(
+            (position) => {
+                const latitude = position.coords.latitude;
+                const longitude = position.coords.longitude;
+                const location = { latitude, longitude };
+                resolve({location : location , err : null});
+            },(error) => {
+                switch (error.code) {
+                    case error.PERMISSION_DENIED:
+                        // return error to the user if permission is denied
+                        resolve({location : null, err : "User denied the request for Geolocation."});
+                        break;
+                    case error.POSITION_UNAVAILABLE:
+                        resolve({location : null, err : "Location information is unavailable."});
+                        break;
+                    case error.TIMEOUT:
+                        resolve({location : null, err : "The request to get user location timed out."});
+                        break;
+                }
+            });
+        }else{
+            resolve({location : null, err : "Geolocation is not supported by this browser."});
+        }
+    })
+}
+
+export {getUserLocation};
