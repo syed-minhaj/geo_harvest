7d71373d52926d348e1b69ec1a11b318eeba97ab
syed-minhaj <minhajsulmuneeb@gmail.com>
2025-08-25 20:48:22 +0500

    style: new font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono , Josefin_Sans} from "next/font/google";
 import "./globals.css";
 import 'leaflet/dist/leaflet.css';
 
@@ -11,6 +11,11 @@ const geistSans = Geist({
 
 const geistMono = Geist_Mono({
     variable: "--font-geist-mono",
+    subsets: ["latin"],
+});
+
+const josefinSans = Josefin_Sans({
+    variable: "--font-josefin-sans",
     subsets: ["latin"],
 });
 
@@ -29,7 +34,7 @@ export default function RootLayout({
     return (
         <html lang="en" >
             <body
-                className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+                className={`${geistSans.variable} ${geistMono.variable} ${josefinSans.variable} antialiased`}
             >
                 {children}
             </body>
