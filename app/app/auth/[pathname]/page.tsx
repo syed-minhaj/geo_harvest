1896cb8b58a0cfc701bd22d3031b4bd574eb171f
syed-minhaj <minhajsulmuneeb@gmail.com>
2025-08-25 21:10:51 +0500

    feat: auth page

---
+++ app/app/auth/[pathname]/page.tsx
@@ -0,0 +1,19 @@
+
+import { authViewPaths } from "@daveyplate/better-auth-ui/server"
+import { AuthCard } from "@daveyplate/better-auth-ui"
+
+export const dynamicParams = false
+
+export function generateStaticParams() {
+    return Object.values(authViewPaths).map((pathname) => ({ pathname }))
+}
+
+export default async function AuthPage({ params }: { params: Promise<{ pathname: string }> }) {
+    const { pathname } = await params
+
+    return (
+        <main className="flex grow  flex-col items-center justify-center gap-3 self-center p-4 md:p-6 mt-4">
+            <AuthCard pathname={pathname} redirectTo="/app"/>
+        </main>
+    )
+}
