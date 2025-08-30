292ff85f8521c6b0465f61faa76afeeca7687694
syed-minhaj <minhajsulmuneeb@gmail.com>
2025-08-30 18:53:03 +0500

    feat : availableCrop data

---
+++ data/crop.ts
@@ -0,0 +1,105 @@
+
+export const availableCrops = {
+    wheat : ["common wheat (Triticum aestivum)", "durum wheat (Triticum durum)", "club wheat (Triticum compactum)"],
+    rice: ["Jasmine Rice" ,"Basmati"],
+    cotton: ["Upland Cotton (Gossypium hirsutum)"],
+}
+
+export const moistureSensitivity = {
+    "wheat" : {
+        "common wheat (Triticum aestivum)" : {
+            period : {
+                1 : 1,
+                2 : 2,
+                3 : 4,
+                4 : 5,
+                5 : 4,
+                6 : 3
+            }
+        },
+        "durum wheat (Triticum durum)" : {
+            period : {
+                1 : 1,
+                2 : 2,
+                3 : 4,
+                4 : 5,
+                5 : 4,
+                6 : 3
+            }
+        },
+        "club wheat (Triticum compactum)" : {
+            period : {
+                1 : 1,
+                2 : 2,
+                3 : 4,
+                4 : 5,
+                5 : 4,
+                6 : 3
+            }
+        },
+        "other" : {
+            period : {
+                1 : 1,
+                2 : 2,
+                3 : 4,
+                4 : 5,
+                5 : 4,
+                6 : 3
+            }
+        }
+    },
+    "rice" : {
+        "Jasmine Rice" : {
+            period : {
+                1 : 1,
+                2 : 2,
+                3 : 4,
+                4 : 5,
+                5 : 4,
+                6 : 3
+            }
+        },
+        "Basmati" : {
+            period : {
+                1 : 1,
+                2 : 2,
+                3 : 4,
+                4 : 5,
+                5 : 4,
+                6 : 3
+            }
+        },
+        "other" : {
+            period : {
+                1 : 1,
+                2 : 2,
+                3 : 4,
+                4 : 5,
+                5 : 4,
+                6 : 3
+            }
+        }
+    },
+    "cotton" : {
+        "Upland Cotton (Gossypium hirsutum)" : {
+            period : {
+                1 : 1,
+                2 : 2,
+                3 : 4,
+                4 : 5,
+                5 : 4,
+                6 : 3
+            }
+        },
+        "other" : {
+            period : {
+                1 : 1,
+                2 : 2,
+                3 : 4,
+                4 : 5,
+                5 : 4,
+                6 : 3
+            }
+        }
+    },
+} ;
