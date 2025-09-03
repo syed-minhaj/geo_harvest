7f2bfbb60638cc3ef848453b8c1301b8b09d95e2
syed-minhaj <minhajsulmuneeb@gmail.com>
2025-09-03 21:50:37 +0500

    fix : update config component

--- app/app/fields/[id]/components/Config.tsx
+++ app/app/fields/[id]/components/Config.tsx
@@ -17,18 +17,14 @@ export default function Config() {
     }
     return (
         <div className='w-full rounded-[1.75rem] border h-96 flex flex-col sm:flex-row gap-4 p-4 flex-wrap'>
-            <button className={`${isActive(hash, "moistureLevel")} border rounded-[0.75rem] p-4 h-fit flex-1`} 
-                    onClick={() => setHash("moistureLevel")}>
-                moistureLevel
-            </button>
-            <button className={`${isActive(hash, "waterRequirement")} border rounded-[0.75rem] p-4 h-fit flex-1`}
-                    onClick={() => setHash("waterRequirement")}>
-                waterRequirement
-            </button>
-            <button className={`${isActive(hash, "cropHealth")} border rounded-[0.75rem] p-4 h-fit flex-1`}
-                    onClick={() => setHash("cropHealth")}>
-                cropHealth
-            </button>
+            {
+                ["waterRequirement" , "nitrogenRequirement" , "phosphorusRequirement" , "cropStress"].map((imageType) => (
+                    <button className={`${isActive(hash, imageType)} border rounded-[0.75rem] p-4 h-fit flex-1`}
+                            onClick={() => setHash(imageType)}>
+                        {imageType}
+                    </button>
+                ))
+            }
         </div>
     )
 }
