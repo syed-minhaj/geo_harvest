5bd3eb595d5001b6f6b69bdc0f7ca39d0187e84b
syed-minhaj <minhajsulmuneeb@gmail.com>
2025-09-16 08:34:07 +0500

    fix : scroll to top when page change ( fixing next.js link )

--- app/components/providers.tsx
+++ app/components/providers.tsx
@@ -2,20 +2,26 @@
 
 import { AuthUIProvider } from "@daveyplate/better-auth-ui"
 import Link from "next/link"
-import { useRouter } from "next/navigation"
+import { usePathname, useRouter } from "next/navigation"
 import type { ReactNode } from "react"
 import { authClient } from "../lib/auth-client"
+import { useEffect } from "react"
 
 export default function Providers({ children }: { children: ReactNode }) {
     const router = useRouter()
+    const pathname = usePathname()
 
+    useEffect(() => {
+        window.scrollTo(0, 0)
+    }, [pathname])
+    
     return (
         <AuthUIProvider
             basePath="/app/auth"
             authClient={authClient}
             navigate={router.push}
             replace={router.replace}
-            redirectTo="/app"
+            redirectTo="/app/fields"
             onSessionChange={() => {
                 router.refresh()
             }}
