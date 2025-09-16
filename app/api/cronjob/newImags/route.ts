232dfd033a081000bbb8935bc08ef2ca4723d332
syed-minhaj <minhajsulmuneeb@gmail.com>
2025-09-16 09:41:27 +0500

    feat  : cron job

---
+++ app/api/cronjob/newImags/route.ts
@@ -0,0 +1,96 @@
+import { NextResponse , NextRequest } from "next/server";
+import { db } from "@/app/lib/drizzle";
+import { field as fieldDB , crop} from "@/db/schema";
+import { supabase } from "@/app/lib/supabase";
+import { sentinel_image , sentinel_catalog } from "@/app/actions/sentinel";
+import { fromPostgresPolygon } from "@/app/utils/coordinate";
+import { eq, sql } from "drizzle-orm";
+import { ImageType } from "@/app/types";
+
+export async function GET(req : NextRequest) {
+    
+    const authHeader = req.headers.get('authorization');
+    if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
+        return new Response('Unauthorized', {
+        status: 401,
+        });
+    }
+    const fields = await db.query.field.findMany({
+        columns : {
+            id : true,
+            coordinates : true,
+            imagesDates : true,
+        },
+        with : {
+            crop : {
+                columns : {
+                    name : true,
+                }
+            }
+        }
+    });
+
+    try{
+        for(const field of fields) {
+            console.log("start" , field.id);
+            const coor = fromPostgresPolygon(field.coordinates);
+            // change coordinates to lat/lng to lng/lat
+            const res = await sentinel_catalog({coordinates : coor.map((c : number[]) => [c[1], c[0]])})
+            if(res.err) {
+                console.log(res.err);
+                return NextResponse.json(
+                    { message : res.err },
+                    { status : 500 }
+                )
+            }
+    
+            const dates = res.data.features.map((f : any) => f.properties.datetime);
+            dates.sort((a:string, b:string) => new Date(b).getTime() - new Date(a).getTime());
+    
+            console.log(dates , "/n" , field.imagesDates);
+            if(field.imagesDates.includes(dates[0])) {
+                console.log("already done" , field.id);
+                continue;
+            }
+            await db.update(fieldDB)
+                .set({
+                    imagesDates: sql`array_append(${fieldDB.imagesDates}, ${dates[0]})`
+                })
+                .where(eq(fieldDB.id, field.id))
+    
+            for(const to  of ["waterRequirement" , "nitrogenRequirement" , "phosphorusRequirement" , "cropStress"] as ImageType[]) {
+            
+                await sentinel_image({coordinates : coor.map((c : number[]) => [c[1], c[0]]) , date:dates[0]  , imageType : to , crop : field.crop[0].name}).then(async (res) => {
+                    if(res.err || res.data === null) {
+                        console.log(res.err);
+                        return null;
+                    }
+                    const { data, error } = await supabase.storage
+                        .from("field")
+                        .upload(`${field.id}/${dates[0]}/${to}.png`, res.data, {
+                            cacheControl: '3600', 
+                            contentType: 'image/png', 
+                            upsert: true, 
+                        });
+    
+                    
+                    if (error) {
+                        console.error('Error uploading image:', error.message);
+                        return null;
+                    }
+                })
+                console.log("done" , to);
+            }
+            console.log("done" , field.id);
+        }
+    }catch(e){
+        console.log(e);
+        return NextResponse.json(
+            { message : "backend error : contact admin." },
+            { status : 500 }
+        )
+    }
+    return NextResponse.json({
+        message: "Cron job completed successfully."
+    });
+}
