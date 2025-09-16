49f49feffd9698ff5046a8d370e499b011ed4a17
syed-minhaj <minhajsulmuneeb@gmail.com>
2025-09-16 09:06:19 +0500

    fix : different redirct

--- app/app/auth/[pathname]/page.tsx
+++ app/app/auth/[pathname]/page.tsx
@@ -13,7 +13,7 @@ export default async function AuthPage({ params }: { params: Promise<{ pathname:
 
     return (
         <main className="flex grow  flex-col items-center justify-center gap-3 self-center p-4 md:p-6 mt-4">
-            <AuthCard pathname={pathname} redirectTo="/app"/>
+            <AuthCard pathname={pathname} redirectTo="/app/fields"/>
         </main>
     )
 }
