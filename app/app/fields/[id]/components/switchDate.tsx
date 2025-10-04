4d7c14ea21d26a133ce44a0434f116fb7cb7eb00
syed-minhaj <minhajsulmuneeb@gmail.com>
2025-10-04 21:53:26 +0500

    fix: switch date section now scroll on overflow

--- app/app/fields/[id]/components/switchDate.tsx
+++ app/app/fields/[id]/components/switchDate.tsx
@@ -5,14 +5,13 @@ import { getDateShort } from "@/app/utils/Date";
 const SwitchDate = ({dates , imagesDate , setImagesDate} : {dates : string[] , imagesDate : string , setImagesDate : React.Dispatch<React.SetStateAction<string>>}) => {
     return (
         <div className='w-full  bottom-0 p-4 absolute object-top z-1000 '>
-            <div className=' w-full h-10  rounded-[0.5rem] text-white bg-[#28282b]/66 flex flex-row-reverse gap-2 p-2  '>
+            <div className=' w-full custom-scrollbar overflow-x-auto rounded-[0.5rem] text-white bg-[#28282b]/66 flex flex-row-reverse gap-2 p-2  '>
                 {dates.map((date , index) => {
-                    console.log(date);
                     return (
                         <>
-                            {index != 0 ? <Separator orientation='vertical' className='bg-white h-auto '/> : null}
-                            <button className={`rounded px-2 w-fit hover:bg-white/33 ${date == imagesDate ? 'bg-white/33' : ''}`} 
-                            onClick={() => setImagesDate(date)}>{getDateShort(new Date(date))}</button>
+                            {index != 0 ? <div><Separator orientation='vertical' className='bg-white '/></div> : null}
+                            <button className={`rounded px-2 w-fit hover:bg-white/33 whitespace-nowrap ${date == imagesDate ? 'bg-white/33' : ''}`} 
+                            onClick={() => setImagesDate(date)} key={index} >{getDateShort(new Date(date))}</button>
                         </>
                 )})}
             </div>
