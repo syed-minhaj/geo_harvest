0cd1d6aa85d77de4a764d41a8a177e03bc454a50
syed-minhaj <minhajsulmuneeb@gmail.com>
2025-10-13 07:03:21 +0500

    feat: manifest for pwa

---
+++ app/manifest.ts
@@ -0,0 +1,20 @@
+import type { MetadataRoute } from 'next'
+ 
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: 'Geo Harvest',
+    short_name: 'GeoHarvest',
+    description: 'GeoHarvest is a web application that allows users monitor their fields using satellite imagery.',
+    start_url: '/app',
+    display: 'standalone',
+    background_color: '#F8FDFB',
+    theme_color: '#F8FDFB',
+    icons: [
+      {
+        src: '/ico.svg',
+        sizes:"any",
+        type: 'image/svg+xml',
+      },
+    ],
+  }
+}
