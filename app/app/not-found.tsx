9daaf4d3a1a5f986ff00233f534b2b01ff4b3a2a
syed-minhaj <minhajsulmuneeb@gmail.com>
2025-10-25 20:07:18 +0500

    feat: update 404 page

---
+++ app/app/not-found.tsx
@@ -0,0 +1,13 @@
+import Link from 'next/link';
+
+export default function NotFound() {
+    return (
+        <>
+            <div className="flex flex-col text-center mt-30 gap-4 mx-4 ">
+                <h2 className="text-3xl">Page Not Found</h2>
+                <p>Could not find the requested page.</p>
+                <Link href="/" className='opacity-66 hover:opacity-100'>Return Home</Link>
+            </div>
+        </>
+    );
+}
