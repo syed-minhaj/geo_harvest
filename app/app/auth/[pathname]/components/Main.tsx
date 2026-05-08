f8eeedd14b96d2919942b4422e6f6d35579b0e9a
syed-minhaj <minhajsulmuneeb@gmail.com>
2026-05-09 03:19:35 +0500

    fix: redirectpath fixed for vercel.app url

--- app/app/auth/[pathname]/components/Main.tsx
+++ app/app/auth/[pathname]/components/Main.tsx
@@ -4,17 +4,16 @@ import { AuthCard } from "@daveyplate/better-auth-ui"
 export default function Main({referrerUrl , pathname}:{referrerUrl:string|null , pathname:string}) {
     
     const redirectPath = 
-        (!referrerUrl || referrerUrl.split("app")[1].includes("auth")) ?
+        (!referrerUrl || referrerUrl.split("/app")[1].includes("auth")) ?
             (localStorage.getItem("referrerUrl")) ?
-                "/app" + localStorage.getItem("referrerUrl")?.split("app")[1]
+                "/app" + localStorage.getItem("referrerUrl")?.split("/app")[1]
             : "app/fields"
         :
-            "/app" + referrerUrl.split("app")[1]
+            "/app" + referrerUrl.split("/app")[1]
         
-    if (referrerUrl && !referrerUrl.split("app")[1].includes("auth")){
+    if (referrerUrl && !referrerUrl.split("/app")[1].includes("auth")){
         localStorage.setItem("referrerUrl", referrerUrl)
     }
-    console.log(redirectPath)
     
     return (
         <main className="flex grow  flex-col items-center justify-center gap-3 self-center p-4 md:p-6 mt-4 w-full">
